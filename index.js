4ea858a44e324ef1208a42e9ee88ddf3949885c4
techkanna <senthilkannanarmy@gmail.com>
2022-12-09 17:37:22 +0530

    added imgs

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ const fs = require('fs');
 const qrcode = require('qrcode-terminal')
 const { Client, MessageMedia } = require('whatsapp-web.js');
 
+const imgURLMap = {
+	senthil: [],
+	venkat: [],
+	mahe: [],
+	senthamil: [],
+	dd: [],
+}
 
 // Use the saved values
 const client = new Client({
-    puppeteer: {
+	puppeteer: {
 		args: ['--no-sandbox'],
 	}
 });
 
 client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true })
+	qrcode.generate(qr, { small: true })
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
-	client.getChats().then(chats=>{
-		const chat = chats.find(ch=>ch.name === 'Web development')
+	console.log('Client is ready!');
+	client.getChats().then(chats => {
+		const chat = chats.find(ch => ch.name === 'Web development')
 		client.sendMessage(chat.id._serialized, "chat bot is now on")
 	})
 });
@@ -25,81 +32,81 @@ client.on('ready', () => {
 // Mention everyone
 client.on('message', async (msg) => {
 
- if (msg.body === '!ping') {
-        // Send a new message as a reply to the current one
-        msg.reply('pong replay');
+	if (msg.body === '!ping') {
+		// Send a new message as a reply to the current one
+		msg.reply('pong replay');
 
-    }else if(msg.body == '!everyone') {
-        const chat = await msg.getChat();
-        
-        let text = "";
-        let mentions = [];
+	} else if (msg.body == '!everyone') {
+		const chat = await msg.getChat();
 
-        for(let participant of chat.participants) {
-            const contact = await client.getContactById(participant.id._serialized);
-            
-            mentions.push(contact);
-            text += `Hello :) @${participant.id.user} ,`;
-        }
+		let text = "";
+		let mentions = [];
 
-        await chat.sendMessage(text, { mentions });
-    }
+		for (let participant of chat.participants) {
+			const contact = await client.getContactById(participant.id._serialized);
+
+			mentions.push(contact);
+			text += `Hello :) @${participant.id.user} ,`;
+		}
+
+		await chat.sendMessage(text, { mentions });
+	}
 });
 
 client.on('message_create', async (msg) => {
-    // Fired on all message creations, including your own
-    if (msg.fromMe) {
-        // do stuff here
-	if (msg.body === '!rn') {
-        	msg.react('👍');
-    	}
+	// Fired on all message creations, including your own
+	if (msg.fromMe) {
+		// do stuff here
+		if (msg.body === '!rn') {
+			msg.react('👍');
+		}
 
-	if(msg.body === '!me'){
-		const chat = await msg.getChat();
-		const url = 'https://techkanna.netlify.app/img/me.jpg'
-		const media = await MessageMedia.fromUrl(url, { caption: 'Here\'s your requested media.' });
-		chat.sendMessage(media);
+		if (msg.body === '!me') {
+			const chat = await msg.getChat();
+			const url = 'https://techkanna.netlify.app/img/me.jpg'
+			const media = await MessageMedia.fromUrl(url, { caption: 'Here\'s your requested media.' });
+			chat.sendMessage(media);
+		}
+
+		if (msg.body == '!everyone') {
+			const chat = await msg.getChat();
+
+			let text = "";
+			let mentions = [];
+
+			for (let participant of chat.participants) {
+				const contact = await client.getContactById(participant.id._serialized);
+
+				mentions.push(contact);
+				text += `Hello :) @${participant.id.user} ,`;
+			}
+
+			await chat.sendMessage(text, { mentions });
+		}
+
 	}
-	
-	if(msg.body == '!everyone') {
-        const chat = await msg.getChat();
-        
-        let text = "";
-        let mentions = [];
 
-        for(let participant of chat.participants) {
-            const contact = await client.getContactById(participant.id._serialized);
-            
-            mentions.push(contact);
-            text += `Hello :) @${participant.id.user} ,`;
-        }
-
-        await chat.sendMessage(text, { mentions });
-    }
-
-    }
-
-	if(msg.body === '!vis'){
+	if (msg.body === '!vis') {
 		const chat = await msg.getChat();
-		
-		const url =  'https://techvenkatvis.github.io/myportfolio/venkat3.jpg'
+
+		const url = 'https://techvenkatvis.github.io/myportfolio/venkat3.jpg'
 
 		const media = await MessageMedia.fromUrl(url);
 		chat.sendMessage(media);
 	}
 
-	if(msg.body === '!vis1'){
+	if (msg.body === '!vis1') {
 		const chat = await msg.getChat();
-		
+
 		const url = 'https://techvenkatvis.github.io/myportfolio/venkat1.jpg'
 		const media = await MessageMedia.fromUrl(url);
 		chat.sendMessage(media);
 	}
 
-	if(msg.body === '!vis2'){
+	if (msg.body === '!vis2') {
 		const chat = await msg.getChat();
-		
-		const url =  'https://techvenkatvis.github.io/myportfolio/venkat222.jpg'
+
+		const url = 'https://techvenkatvis.github.io/myportfolio/venkat222.jpg'
 
 		const media = await MessageMedia.fromUrl(url);
 		chat.sendMessage(media);
@@ -108,11 +115,11 @@ client.on('message_create', async (msg) => {
 
 
 client.on('change_state', state => {
-    console.log('CHANGE STATE', state );
+	console.log('CHANGE STATE', state);
 });
 
 client.on('disconnected', (reason) => {
-    console.log('Client was logged out', reason);
+	console.log('Client was logged out', reason);
 });
 
 
